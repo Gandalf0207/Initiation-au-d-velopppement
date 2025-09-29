592284411db6cc363aaab9e752835940f8b2caaf
Gandalf0207 <quentin.pladeau@gmail.com>
2025-09-29 09:33:13 +0200

    avanccement tp 1

--- TP1/EXO1.java
+++ TP1/EXO1.java
@@ -5,19 +5,23 @@ public class EXO1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        EXO1.suiteOrdre(n);
+        double epsi = sc.nextDouble();
+        EXO1.suiteOrOrdre(n);
+        EXO1.suiteOrEpsilon(epsi);
     }
 
     public static double  Fibo(int n) {
-        if(n == 1 || n == 2) {
-            return 1 ; 
-        } else {
-            return EXO1.Fibo(n-1) + EXO1.Fibo(n-2);
+
+        int a = 0; int b = 1;
+        for(int i = 0; i < n; i++) {
+            int temp = a;
+            a = b;
+            b = temp+b;
         }
+        return a;
     }
 
-
-    public static void suiteOrdre(int n) {
+    public static void suiteOrOrdre(int n) {
         if(n > 2) {
             for(int i = 2; i <= n; i++) {
                 double Vi = EXO1.Fibo(i) / EXO1.Fibo(i-1);
@@ -28,5 +32,20 @@ public class EXO1 {
                 System.out.println("Valeur de P(Vi) : " + PVi);
             }
         }
+    }
+
+    public static void suiteOrEpsilon(double epsi) {
+        int i = 2; double Vi; double PVi;
+
+        do {
+            Vi = EXO1.Fibo(i) / EXO1.Fibo(i-1);
+            PVi = Vi*Vi - Vi -1;
+
+            System.out.println("Valeur de i : " + i);
+            System.out.println("Valeur de Vi : " + Vi);
+            System.out.println("Valeur de P(Vi) : " + PVi);
+
+            i++;
+        } while(Math.abs(Vi-Math.sqrt(Math.abs(PVi))) > epsi); // marche pas
     }
 }
