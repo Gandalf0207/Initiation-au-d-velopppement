6195ed18281ae2908b96330b17bbbef8ae4c7577
Kentus <quentin.pladeau@gmail.com>
2025-10-10 14:41:29 +0200

    Fin exo 2 tp2

--- TP2/TP2Main.java
+++ TP2/TP2Main.java
@@ -1,9 +1,11 @@
 package TP2;
-
 import java.util.Random;
 
 public class TP2Main {
     public static void main(String[] args) {
+        int [] planche = {20, 3};
+        boolean estArrive = TP2Main.arivobato(planche, true);
+        System.out.println(estArrive);
         // TP2Main.TestProbaGenerationAleatoire();
     }
 
@@ -33,7 +35,49 @@ public class TP2Main {
         System.out.println("environ " + (s3*100.0)/1000 + "% de ces nombres sont compris entre 71 et 90");
     }
 
-    public static boolean arivobato(int [] plancheTaille) {
+    public static boolean arivobato(int [] plancheTaille, boolean trace) {
+        int longueur = plancheTaille[0];
+        int largeur = plancheTaille[1];
+        int x = 1; int y = 0;
+
+        while ((y != longueur) && (-1 < x)  && (x < largeur)) {
+            int value = TP2Main.GenerationAleatoire();
+            if (value < 51) {
+                y ++;
+            } else if (value < 71) {
+                x++;
+            } else if (value < 91) {
+                x--;
+            } else {
+                y--;
+            }
+
+            if( y < 0 ){
+                y = 0;
+            }
+            if (trace) {
+                TP2Main.affichePlanche(longueur, largeur, x, y);
+            }
+        }
+
+        return y==longueur;
+    }
+
+    public static void affichePlanche(int longueur, int largeur, int x, int y){
+        System.out.print("\n\n\n");
+
+        for(int i=0; i< largeur; i++) {
+            for (int j=0; j < longueur; j++) {
+                String c = (i==x && j ==y) ? "O":"-";
+                System.out.print(c + " ");
+            }
+            System.out.println();
+        }
+        try {
+            Thread.sleep(250);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
