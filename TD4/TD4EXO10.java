b13540ec8bcfa80bcf02f5181215076502676ba4
Kentus <quentin.pladeau@gmail.com>
2025-10-16 14:33:07 +0200

    patch exo 9 et 10 td4

--- TD4/TD4EXO10.java
+++ TD4/TD4EXO10.java
@@ -5,27 +5,54 @@ public class TD4EXO10 {
     public static void main(String[] args) {
         int[] t1 = {3, 5, 8};
         int[] t2 = {5, 7, 3, 9};
-        ArrayList<Integer> newTab = TD4EXO10.fusionSansDoublon(t1, t2);
+        int[] newTab = TD4EXO10.fusionSansDoublon(t1, t2);
 
         for(Integer elt:newTab) {
             System.out.print(elt + " ");
         }
     }
 
-    public static ArrayList<Integer> fusionSansDoublon(int[] t1, int[] t2) {
-        int[] newTab = new int[t1.length + t2.length];
-        for(int i = 0; i < t1.length; i++){
-            newTab[i] = t1[i];
+    public static int[] fusionSansDoublon(int[] t1, int[] t2) {
+
+        //concaténation dans un nouveau tableau de T1 et T2
+        int[]fullTab = new int[t1.length + t2.length];
+        for(int i = 0; i< t1.length; i++) {
+            fullTab[i] = t1[i];
+        }
+        for(int i = t1.length; i<(t1.length + t2.length); i++) {
+            fullTab[i] = t2[i- t1.length];
         }
 
-        for(int i = 0; i < t2.length; i++){
-            newTab[t1.length +i] = t2[i];
+        // calcul nombre :
+        int nb = 0;
+        int dixPuissance = 1;
+        for(int i = fullTab.length-1; i > -1; i--) {
+            nb += fullTab[i]*dixPuissance;
+            dixPuissance*= 10;
         }
 
+        // get fréquence des chiffres
+        int[] freq = EXO3.frequencesChiffres(nb);
 
-        newTab = TD4EXO5.triSelection(newTab);
-        ArrayList<Integer> newTab2 = TD4EXO9.compresser(newTab);
+        // calcul nouvelle longueur tableau final
+        int cpt = 0;
+        for(int elt:freq) {
+            if(elt > 1) {
+                cpt += elt-1;
+            }
+        }
 
-        return  newTab2;
+        int[] finalTab = new int[fullTab.length-cpt];
+        cpt = finalTab.length-1;
+        for(int i = fullTab.length-1; i> -1; i--) {
+            if(freq[fullTab[i]] > 1) {
+                freq[fullTab[i]] --;
+            } else {
+                finalTab[cpt] = fullTab[i];
+                cpt --;
+            }
+        }
+
+        return  finalTab;
     }
 }
--- TD4/TD4EXO9.java
+++ TD4/TD4EXO9.java
@@ -3,21 +3,34 @@ import java.util.ArrayList;
 public class TD4EXO9 {
     public static void main(String[] args) {
         int[] tab = {4,4,4,2,2,3,4,4};
-        ArrayList<Integer> newTb = TD4EXO9.compresser(tab);
+        int[] newTb = TD4EXO9.compresser(tab);
 
         for(int elt:newTb){
             System.out.print(elt + " ");
         }
     }
 
-    public static ArrayList<Integer> compresser(int[] tab) {
-        ArrayList<Integer> newTab = new ArrayList<>();
-        newTab.add(tab[0]);
+    public static int[] compresser(int[] tab) {
 
+        int cpt = 0;
+        int lastNum = tab[0];
+        for(int i = 1; i<tab.length; i++) {
+            if (tab[i] == lastNum) {
+                cpt ++;
+            } else {
+                lastNum = tab[i];
+            }
+        }
 
-        for (int i = 1; i < tab.length; i++) {
-            if (tab[i] != newTab.get(newTab.size() - 1)) {
-                newTab.add(tab[i]);
+        int[] newTab = new int[tab.length-cpt];
+        newTab[0] = tab[0];
+        cpt = 1;
+        lastNum = tab[0];
+        for(int i = 1; i < tab.length; i++) {
+            if(tab[i] != lastNum) {
+                newTab[cpt] = tab[i];
+                lastNum = tab[i];
+                cpt ++;
             }
         }
 
