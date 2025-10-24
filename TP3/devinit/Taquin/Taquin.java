f3e3db9692979efde6c00709b1970139aa800d89
Kentus <quentin.pladeau@gmail.com>
2025-10-24 09:38:44 +0200

    TP3 exo 10 final toujours pas fonctionnel

--- TP3/devinit/Taquin/Taquin.java
+++ TP3/devinit/Taquin/Taquin.java
@@ -213,37 +213,51 @@ public class Taquin {
 
     }
 
-    // créer le tableau dans le sens zig zag
-    // calculer nb transposition de tout les nombres
-    // calculer nb transposition de la position 0 initiale et l'arrivée
-    // comparé la parité de ses deux nombre :
+    // créer le tableau dans le sens zig zag   ok
+    // calculer nb transposition de tous les nombres    ok
+    // calculer nb transposition de la position 0 initiale et l'arrivée    ok
+    // comparé la parité de ses deux nombres :
         // si égale alors soluble
         // si pas égale alors pas soluble
 
     public static boolean estGrilleResoluble(int[][] grille) {
-        int sommeRetar = 0;
 
         int [] fullInTab = new int[grille.length*grille.length];
 
+        // formation tableau zigzag depuis la matrice
         for(int i = 0; i < grille.length; i++) {
+            for(int j =0, k = grille.length-1; j < grille.length; j++, k--) {
 
-            if(i%2 == 0) {
-                for(int j =0; j < grille.length; j++) {
+                if(i%2==0) {
                     if (grille[i][j] != 0) {
                         fullInTab[grille.length*i + j] = grille[i][j];
                     } else {
                         fullInTab[grille.length*i + j] = grille.length*grille.length;
                     }
-                }
-            } else {
-                for(int j =grille.length-1; j > -1; j--) {
-                    if (grille[i][j] != 0) {
-                        fullInTab[grille.length*i - j] = grille[i][j];
+                } else {
+                    if (grille[i][k] != 0) {
+                        fullInTab[grille.length*i + j] = grille[i][k];
                     } else {
-                        fullInTab[grille.length*i - j] = grille.length*grille.length;
+                        fullInTab[grille.length*i + j] = grille.length*grille.length;
                     }
                 }
             }
+        }
+
+        // calcul du nombre de transpositions pour chaque nombre (retard des éléments
+        int sommeRetar = 0;
+        for(int i = 0; i<fullInTab.length; i++) {
+            for (int elt = i; elt <fullInTab.length; elt++) {
+                if(fullInTab[i] > fullInTab[elt]) {
+                    sommeRetar ++;
+                }
+            }
+        }
+
+        // calcul nb transposition qu'il faut au zero pour aller à la fin
+        int cpt = 0;
+        for(int i = 0; i < fullInTab.length; i++) {
+            cpt = fullInTab.length - i - 1; // le -1 c'est parce qu'on ne compte pas la place actuelle
         }
 
         System.out.println();
@@ -253,18 +267,7 @@ public class Taquin {
         System.out.println();
         System.out.println();
 
-        int cpt;
-        for(int i = 0; i<fullInTab.length; i++) {
-            cpt = 0;
-            for (int j = i; j < fullInTab.length; j++) {
-                if(fullInTab[j] < fullInTab[i]) {
-                    cpt++;
-                }
-            }
-            sommeRetar += cpt;
-        }
-
-        return sommeRetar%2==0;
+        return ((sommeRetar%2 == 0) == (cpt%2==0));
 
 
     }
--- UseFul/Matrice.java
+++ UseFul/Matrice.java
@@ -14,5 +14,5 @@ public class Matrice {
             }
             System.out.println();
         }
-    }
+            }
 }
